508e453d2a097c555c6ce089ac49163586b1358d
dorara@work <satosinisemono@gmail.com>
2019-05-29 15:35:57 +0900

    [add] top　scrollMagicを導入,cssアニメーションを改修

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,33 @@
-console.log('Re:may');
+//hello
+console.log('Welcome, Re:may');
+
+// load
+$(function() {
+  setTimeout(() => {
+    $('.p-top__title p').addClass('active');
+  }, 1500);
+});
+
+// scroll in animation
+var controller = new ScrollMagic.Controller();
+
+new ScrollMagic.Scene({triggerElement: ".p-intro"})
+  .on("enter", function (event) {
+    $('.p-intro').addClass('active');
+  })
+  .addTo(controller);
+new ScrollMagic.Scene({triggerElement: ".p-works"})
+  .on("enter", function (event) {
+    $('.p-works').addClass('active');
+  })
+  .addTo(controller);
+new ScrollMagic.Scene({triggerElement: ".p-skills"})
+  .on("enter", function (event) {
+    $('.p-skills').addClass('active');
+  })
+  .addTo(controller);
+new ScrollMagic.Scene({triggerElement: ".p-links"})
+  .on("enter", function (event) {
+    $('.p-links').addClass('active');
+  })
+  .addTo(controller);
