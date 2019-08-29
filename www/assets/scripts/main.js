adb3a7f5fa812f92d5735d593c85f30556731215
dorara <satosinisemono@gmail.com>
2019-08-29 23:55:19 +0900

    add: css-animation

--- www/assets/scripts/main.js
+++ www/assets/scripts/main.js
@@ -1,33 +1,31 @@
 //hello
 console.log('Welcome, Re:may');
 
-// load
-$(function() {
-  setTimeout(() => {
-    $('.p-top__title p').addClass('active');
-  }, 700);
-});
+setTimeout(() => {
+  $('#js-anime').hide();
+  $('#js-content').show();
+}, 4000);
 
 // scroll in animation
-var controller = new ScrollMagic.Controller();
+// var controller = new ScrollMagic.Controller();
 
-new ScrollMagic.Scene({triggerElement: ".p-intro"})
-  .on("enter", function (event) {
-    $('.p-about').addClass('active');
-  })
-  .addTo(controller);
-new ScrollMagic.Scene({triggerElement: ".p-works"})
-  .on("enter", function (event) {
-    $('.p-works').addClass('active');
-  })
-  .addTo(controller);
-new ScrollMagic.Scene({triggerElement: ".p-skills"})
-  .on("enter", function (event) {
-    $('.p-skills').addClass('active');
-  })
-  .addTo(controller);
-new ScrollMagic.Scene({triggerElement: ".p-links"})
-  .on("enter", function (event) {
-    $('.p-links').addClass('active');
-  })
-  .addTo(controller);
+// new ScrollMagic.Scene({triggerElement: ".p-about"})
+//   .on("enter", function (event) {
+//     $('.p-about').addClass('active');
+//   })
+//   .addTo(controller);
+// new ScrollMagic.Scene({triggerElement: ".p-works"})
+//   .on("enter", function (event) {
+//     $('.p-works').addClass('active');
+//   })
+//   .addTo(controller);
+// new ScrollMagic.Scene({triggerElement: ".p-skills"})
+//   .on("enter", function (event) {
+//     $('.p-skills').addClass('active');
+//   })
+//   .addTo(controller);
+// new ScrollMagic.Scene({triggerElement: ".p-links"})
+//   .on("enter", function (event) {
+//     $('.p-links').addClass('active');
+//   })
+//   .addTo(controller);
